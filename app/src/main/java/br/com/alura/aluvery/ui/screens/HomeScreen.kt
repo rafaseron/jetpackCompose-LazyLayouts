3fdd0db55800349d2f328c835368aac1dcb66cdb
rafaseron <rafaelseron3011@gmail.com>
2024-02-22 16:09:22 -0400

    inicio do projeto

---
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -0,0 +1,50 @@
+package br.com.alura.aluvery.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.alura.aluvery.model.Product
+import br.com.alura.aluvery.sampledata.sampleSections
+import br.com.alura.aluvery.ui.components.ProductsSection
+import br.com.alura.aluvery.ui.theme.AluveryTheme
+
+@Composable
+fun HomeScreen(
+    sections: Map<String, List<Product>>
+) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Spacer(Modifier)
+        for (section in sections) {
+            val title = section.key
+            val products = section.value
+            ProductsSection(
+                title = title,
+                products = products
+            )
+        }
+        Spacer(Modifier)
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    AluveryTheme {
+        Surface {
+            HomeScreen(sampleSections)
+        }
+    }
+}
