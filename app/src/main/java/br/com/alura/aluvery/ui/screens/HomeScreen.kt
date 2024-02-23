69455de9d715f0039d3efecb13e5f81e82dcaea4
rafaseron <rafaelseron3011@gmail.com>
2024-02-23 14:35:20 -0400

    leadingIcon placeholder

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package br.com.alura.aluvery.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,31 +11,37 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.sampledata.sampleSections
 import br.com.alura.aluvery.ui.components.ProductsSection
 import br.com.alura.aluvery.ui.theme.AluveryTheme
+import br.com.alura.aluvery.R
 
 @Composable
 fun HomeScreen(sections: Map<String, List<Product>>) {
 
     Column {
-        var texto by remember { mutableStateOf("Produto") }
+        var texto by remember { mutableStateOf("") }
 
         OutlinedTextField(value = texto,
             onValueChange = { newValue -> texto = newValue},
 
-            modifier = Modifier.fillMaxWidth(1f)
-            .padding(start = 30.dp, top = 16.dp, end = 30.dp),
-            shape = RoundedCornerShape(percent = 100))
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(start = 30.dp, top = 16.dp, end = 30.dp),
+            shape = RoundedCornerShape(percent = 100),
+            leadingIcon = {Image(painter = painterResource(id = R.drawable.baseline_search_24), contentDescription = "Search")},
+            placeholder = { Text(text = "O que você procura?")})
 
         /* Modo de cima é didatico para mostrar que o newValue armazena
          o novo valor serve para 'repassar' esse novo valor para o texto */
