3ccf12a23a6f251cb1572eea103c899dd3bc2b85
rafaseron <rafaelseron3011@gmail.com>
2024-02-22 22:16:48 -0400

    Refatoracao com codigo Alura

--- app/src/main/java/br/com/alura/aluvery/ui/screens/AllProducts.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/AllProducts.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -28,21 +30,21 @@ fun AllProducts() {
             start = 16.dp, end = 16.dp, bottom = 90.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
-        columns = GridCells.Adaptive(minSize = 170.dp)) {
+        columns = GridCells.Fixed(2)) {
 
-        item {
-            Text(text = "Todos", fontSize = 30.sp,
+        item(span = { GridItemSpan(maxLineSpan) }){
+            Text(text = "Todos produtos", fontSize = 30.sp,
                 fontWeight = FontWeight(400),
-                modifier = Modifier.offset(x = 16.dp)) }
+                modifier = Modifier.offset(x = 16.dp))
+        }
 
-        item {
-            Text(text = "produtos", fontSize = 30.sp,
-            fontWeight = FontWeight(400),
-            modifier = Modifier.offset(x = -80.dp)) }
-
-        for (produto in todosProdutos){
+        /*for (produto in todosProdutos){
             item {
                 ProductItem(product = produto) }
+        }*/
+
+        items(todosProdutos){p ->
+            ProductItem(product = p)
         }
     }
 }
