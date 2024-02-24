d8cd40a8a786b3ebeb06d3ce0ab3084d39abf7a6
rafaseron <rafaelseron3011@gmail.com>
2024-02-24 15:28:21 -0400

    SearchSelection

---
+++ app/src/main/java/br/com/alura/aluvery/ui/components/SearchSelection.kt
@@ -0,0 +1,41 @@
+package br.com.alura.aluvery.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.alura.aluvery.model.Product
+import br.com.alura.aluvery.sampledata.todosProdutos
+import br.com.alura.aluvery.ui.theme.AluveryTheme
+
+@Composable
+fun SearchSelection(cardlist: List<Product>) {
+
+    LazyColumn(modifier = Modifier, contentPadding = PaddingValues(all = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)){
+        items(cardlist){
+            p ->
+            CardProductItem(product = p)
+        }
+
+    }
+
+}
+
+@Preview
+@Composable
+fun SearchSelectionPreview() {
+    Surface {
+        AluveryTheme {
+            SearchSelection(todosProdutos)
+        }
+    }
+}
