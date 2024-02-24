8d23eeb528a05a60cde6d68b3bb85ceb794b71e8
rafaseron <rafaelseron3011@gmail.com>
2024-02-24 15:45:24 -0400

    retirar imports desnecessarios

--- app/src/main/java/br/com/alura/aluvery/ui/components/SearchSelection.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/components/SearchSelection.kt
@@ -1,13 +1,9 @@
 package br.com.alura.aluvery.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
