5a6e06a76015705ef8d61aa168b844d7c4ebe5aa
rafaseron <rafaelseron3011@gmail.com>
2024-02-28 17:31:49 -0400

    class to data class

--- app/src/main/java/br/com/alura/aluvery/model/Product.kt
+++ app/src/main/java/br/com/alura/aluvery/model/Product.kt
@@ -2,5 +2,5 @@ package br.com.alura.aluvery.model
 
 import java.math.BigDecimal
 
-class Product(val name: String, val price: BigDecimal,
+data class Product(val name: String, val price: BigDecimal,
     val image: String? = null, val description: String? = null)
