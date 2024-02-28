1ec57b9872204acf36e10548d000a2e0020b6b64
rafaseron <rafaelseron3011@gmail.com>
2024-02-28 19:03:11 -0400

    ContentScale to AsyncImage

--- app/src/main/java/br/com/alura/aluvery/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/activities/ProductFormActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
@@ -78,7 +79,7 @@ fun ProductFormScreen() {
         if (urlImagem.isBlank()){} else{
             AsyncImage(model = urlImagem, contentDescription = null, modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp))
+                .height(200.dp), contentScale = ContentScale.Fit)
         }
 
         OutlinedTextField(value = urlImagem, onValueChange = {newValue ->
