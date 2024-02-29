19b3246b055c43790655b58048ebd866ae2f61b9
rafaseron <rafaelseron3011@gmail.com>
2024-02-28 21:50:06 -0400

    FloatActionButton na Main

--- app/src/main/java/br/com/alura/aluvery/activities/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/activities/MainActivity.kt
@@ -3,11 +3,20 @@ package br.com.alura.aluvery.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.alura.aluvery.sampledata.sampleSections
 import br.com.alura.aluvery.ui.screens.HomeScreen
 import br.com.alura.aluvery.ui.theme.AluveryTheme
+import br.com.alura.aluvery.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +26,20 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun App() {
     AluveryTheme {
         Surface {
-            HomeScreen(sections = sampleSections)
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = {  }) {
+                    Icon(painter = painterResource(id = R.drawable.baseline_add_24), contentDescription = "add")
+                }
+            }) {paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)){
+                    HomeScreen(sections = sampleSections)
+                }
+
+            }
             //SearchScreen(selections = sampleSections)
         }
     }
