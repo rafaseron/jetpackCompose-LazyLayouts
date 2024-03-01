5e1eed89763bf24b25be3c61f0714b7b959a7abc
rafaseron <rafaelseron3011@gmail.com>
2024-02-29 20:40:04 -0400

    funcao de save no Dao

--- app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
+++ app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
@@ -1,10 +1,15 @@
 package br.com.alura.aluvery.dao
 
+import androidx.compose.runtime.mutableStateListOf
+import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.sampledata.todosProdutos
 
 class ProductDao {
     companion object{
-        private val mutableProdutos = todosProdutos.toMutableList()
+        private val mutableProdutos =  mutableStateListOf<Product>(*todosProdutos.toTypedArray())
+    }
+    fun save(produto: Product){
+        mutableProdutos.add(produto)
     }
     fun listProducts() = mutableProdutos.toList()
 }
