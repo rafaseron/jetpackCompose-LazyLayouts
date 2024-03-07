5723393dfee0c017bf4b5620acfd6f445211740b
rafaseron <rafaelseron3011@gmail.com>
2024-03-07 15:28:28 -0400

    rememberSaveable para texto

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -46,7 +47,7 @@ fun HomeScreen(daoList:List<Product>) {
         "Bebidas" to sampleDrinks
     )
 
-    var texto by remember { mutableStateOf("") }
+    var texto by rememberSaveable { mutableStateOf("") }
 
     fun alterarTexto(novoValor: String){
         texto = novoValor
