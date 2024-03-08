dc30221e3d0b7d0be3f13d3e2cb2e4cbc06e449d
rafaseron <rafaelseron3011@gmail.com>
2024-03-08 06:19:10 -0400

    MutableStateFlow no DAO

--- app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
+++ app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
@@ -3,13 +3,20 @@ package br.com.alura.aluvery.dao
 import androidx.compose.runtime.mutableStateListOf
 import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.sampledata.todosProdutos
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class ProductDao {
     companion object{
-        private val mutableProdutos =  mutableStateListOf<Product>(*todosProdutos.toTypedArray())
+        private val mutableProdutos =  MutableStateFlow<List<Product>>(value = todosProdutos)
     }
     fun save(produto: Product){
-        mutableProdutos.add(produto)
+        mutableProdutos.value = mutableProdutos.value + produto
     }
-    fun listProducts() = mutableProdutos.toList()
+    fun listProducts(): StateFlow<List<Product>> {
+        return mutableProdutos.asStateFlow()
+    }
+    //aqui convertemos a lista de 'Mutable'StateFlow para 'apenas' StateFlow
+    //basicamente tipo um private set
 }
