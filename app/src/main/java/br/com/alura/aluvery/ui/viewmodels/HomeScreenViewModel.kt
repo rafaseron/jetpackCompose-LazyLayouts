3d736d2a896fed99137f1463feb05e46683a17dd
rafaseron <rafaelseron3011@gmail.com>
2024-03-08 06:37:34 -0400

    CoroutineScope pro .collect

--- app/src/main/java/br/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/viewmodels/HomeScreenViewModel.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import br.com.alura.aluvery.dao.ProductDao
 import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.sampledata.sampleCandies
 import br.com.alura.aluvery.sampledata.sampleDrinks
 import br.com.alura.aluvery.sampledata.sampleProducts
+import kotlinx.coroutines.launch
 
 data class HomeScreenUiState(val sections: Map<String, List<Product>> = emptyMap(),
                         val produtosPesquisados: List<Product> = emptyList(),
@@ -23,20 +25,30 @@ class HomeScreenViewModel: ViewModel(){
     private val dao = ProductDao()
     val daoList = dao.listProducts()
 
-    val mapSections:Map<String, List<Product>> = mapOf(
-        "Todos produtos" to daoList,
-        "Salgados" to sampleProducts,
-        "Doces" to sampleCandies,
-        "Bebidas" to sampleDrinks
-    )
-
     var uiState: HomeScreenUiState by mutableStateOf(HomeScreenUiState(
         onSearchChange = {
-            uiState = uiState.copy(texto = it, produtosPesquisados = searchedProducts(it))},
-        sections = mapSections)
-
+            uiState = uiState.copy(texto = it, produtosPesquisados = searchedProducts(it) )
+        } )
     )
         private set
+
+    init {
+        //como o .collect é uma Coroutine, precisamos colocar dentro desse Escopo para que seja executado
+        viewModelScope.launch {
+
+            //usa-se o collect para conseguir pegar o valor de daoList (que tem o type de StateFlow<List<Product>>)
+            daoList.collect{listaColetada ->
+                uiState = uiState.copy(
+                    sections = mapOf(
+                        "Todos produtos" to listaColetada,
+                        "Salgados" to sampleProducts,
+                        "Doces" to sampleCandies,
+                        "Bebidas" to sampleDrinks)
+                )
+            }
+
+        }
+    }
 
     private fun getSearchedProducts(texto: String): List<Product>{
         return daoList.filter { p ->
