d5976b9ed10dbcdd400ed4caec8435f663e94b2f
rafaseron <rafaelseron3011@gmail.com>
2024-03-11 23:45:35 -0400

    remover imports

--- app/src/main/java/br/com/alura/aluvery/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/activities/ProductFormActivity.kt
@@ -22,10 +22,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
